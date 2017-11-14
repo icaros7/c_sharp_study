fb7105de527c2cff824a0b6c8ca03feb15089ddd
iCAROS7 <hominlab@gmail.com>
2017-11-14 12:39:27 +0900

    1711141239 Constructor

---
+++ Constructor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("생성자 호출 전");
+            Person person = new Person();
+            Console.WriteLine("생성자 호출 후");
+        }
+    }
+    class Person
+    {
+        string name;
+
+        public Person()
+        {
+            name = "Homin Rhee";
+            Console.WriteLine("생성자 호출 완료");
+        }
+    }
+}
