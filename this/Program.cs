de695153f9c0920d355f21fb43377961dea014b5
iCAROS7 <hominlab@gmail.com>
2018-04-05 12:41:44 +0900

    201804051241

---
+++ this/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @this
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A(50);
+            a.output();
+
+        }
+    }
+
+    class A
+    {
+        private int num;
+        public A(int num)
+        {
+            this.num = num;
+        }
+        public void output()
+        {
+            Console.WriteLine("num : " + num);
+        }
+    }
+}
