eb8b9da6d430d61eab3f653e77741a63cc5482d5
iCAROS7 <hominlab@gmail.com>
2018-04-07 17:20:57 +0900

    1804071720

---
+++ MethodOverload/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathmatics homin = new Mathmatics();
+            Console.WriteLine(homin.Abs(-10));
+            Console.WriteLine(homin.Abs(-10.2234));
+            Console.WriteLine(homin.Abs(-50.51m));
+        }
+    }
+
+    class Mathmatics
+    {
+        public int Abs(int value) { return (value >= 0) ? value : -value; }
+        public double Abs (double value) { return (value >= 0) ? value : -value; }
+        public decimal Abs (decimal value) { return (value >= 0) ? value : -value; }
+    }
+}
