913608a7c0d9ac74fbda6be4da1c06dc6e7efaf7
iCAROS7 <hominlab@gmail.com>
2018-05-08 21:13:06 +0900

    180508211304

---
+++ MinMax/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MinMax
+{
+    class MainClass
+    {
+        private static void Main(string[] args)
+        {
+            int i = Int32.MaxValue;
+            int ii = Int32.MinValue;
+            Console.WriteLine("i = {0}\nii = {1}", i, ii);
+        }
+    }
+}
---
+++ MinMax/Structor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Structor
+{
+    class MainClass
+    {
+        struct Point
+        {
+            public int x;
+            public int y;
+
+            public Point(int X, int Y)
+            {
+                this.x = X;
+                this.y = Y;
+            }
+            public override string ToString()
+            {
+                return string.Format("{0}, {1}", x, y);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Point pt = new Point(10, 2);
+            Console.WriteLine(pt.ToString());
+        }
+    }
+}
