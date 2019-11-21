447357d5973360afab1f196ca6353dd81bf39e27
Homin Rhee <hominlab@minnote.net>
2019-11-21 23:30:43 +0900

    191121 Heap: Boxing

---
+++ Heap/Boxing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Heap
+{
+    public class App
+    {
+        public static void Main(string[] args)
+        {
+	        int a = 5;
+	        int b = 6;
+	        
+	        int c = GetMaxValue(a, b);
+        }
+        
+        private static int GetMaxValue(object n1, object n2) // Boxing.
+        {
+            int a = (int)n1; // UnBoxing.
+            int b = (int)n2; // UnBoxing.
+            
+            if (a >= b)
+            {
+                return a;
+            }
+            
+            return b;
+        }
+    }
+}
