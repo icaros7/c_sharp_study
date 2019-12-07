0a3e612bdb60b694637e2eef730f5e54369f0322
iCAROS7 <hominlab@minnote.net>
2019-12-07 23:59:51 +0900

    191207 비관리 자원 #2

---
+++ unmanaged/BadProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace unmanaged
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		while (true)
+		{
+			UnmanagedMemoryManager m = new UnmanagedMemoryManager();
+			m = null;
+			GC.Collect();
+
+			Console.WriteLine(Process.GetCurrentProcess().PrivateMemorySize64);
+		}
+        }
+    }
+
+    class UnmanagedMemoryManager
+    {
+	IntPtr pBuffer;
+
+	public UnmanagedMemoryManager()
+	{
+		pBuffer = Marshal.AllocCoTaskMem(4096 * 2048);
+	}
+    }
+}
--- unmanaged/Program.cs
+++ unmanaged/Program.cs
@@ -1,31 +1,46 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 
-namespace unmanaged
+namespace Unman
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+class Program
+{
+	static void Main(string[] args)
+	{
 		while (true)
 		{
 			UnmanagedMemoryManager m = new UnmanagedMemoryManager();
 			m = null;
-			GC.Collect();
 
+			GC.Collect();
 			Console.WriteLine(Process.GetCurrentProcess().PrivateMemorySize64);
 		}
-        }
-    }
+	}
+}
 
-    class UnmanagedMemoryManager
-    {
+class UnmanagedMemoryManager
+{
 	IntPtr pBuffer;
+	bool _disposed;
 
 	public UnmanagedMemoryManager()
 	{
-		pBuffer = Marshal.AllocCoTaskMem(4096 * 2048);
+		pBuffer = Marshal.AllocCoTaskMem(4096 * 1024);
 	}
-    }
+
+	public void Dispose()
+	{
+		if (_disposed == false)
+		{
+			Marshal.FreeCoTaskMem(pBuffer);
+			_disposed = true;
+		}
+	}
+
+	~UnmanagedMemoryManager()
+	{
+		Dispose();
+	}
+}
 }
