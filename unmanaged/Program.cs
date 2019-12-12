5b5eca3204564f8962104c99e07290564a0074b0
iCAROS7 <hominlab@minnote.net>
2019-12-12 22:45:27 +0900

    191212 비관리 자원 종료 큐와 GC의 이해

--- unmanaged/Program.cs
+++ unmanaged/Program.cs
@@ -29,18 +29,29 @@ class UnmanagedMemoryManager
 		pBuffer = Marshal.AllocCoTaskMem(4096 * 1024);
 	}
 
-	public void Dispose()
+	public void Dispose(bool disposing)
 	{
 		if (_disposed == false)
 		{
 			Marshal.FreeCoTaskMem(pBuffer);
 			_disposed = true;
 		}
+
+		if (disposing == false)
+		{
+			// disposing이 false인 경우 명시적으로 Dispose()를 호출한 경우다. ㄸㅏ라서 종료 큐에서 자신을 제거하여 GC의 부담 감소.
+			GC.SuppressFinalize(this);
+		}
+	}
+
+	public void Dispose()
+	{
+		Dispose(false);
 	}
 
 	~UnmanagedMemoryManager()
 	{
-		Dispose();
+		Dispose(true);
 	}
 }
 }
