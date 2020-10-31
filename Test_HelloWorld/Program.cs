42ed194082eb731f1c5f22747d63f155a66422db
iCAROS7 <hominlab@gmail.com>
2020-11-01 03:19:37 +0900

    테스트 프로젝트 생성

---
+++ Test_HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
