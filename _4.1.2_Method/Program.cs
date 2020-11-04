24b02be66039856b5037ed89d4086613cb7fc374
iCAROS7 <hominlab@gmail.com>
2020-11-04 20:07:00 +0900

    4.1.2장 메서드 예제

---
+++ _4.1.2_Method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Math
+{
+class Math
+{
+    public int f(int x)
+    {
+        return x * x;
+    }
+}
+
+class Program
+{
+    static void Main(string[] Args)
+    {
+        var m = new Math();
+        int result = m.f(5);
+        Console.WriteLine($"5^2 = " + result);
+    }
+}
+}
