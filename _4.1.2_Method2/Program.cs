d6c4c93104cd2bc8e79a57f289ad510e59190944
iCAROS7 <hominlab@gmail.com>
2020-11-04 20:14:47 +0900

    4.1.2장 메서드 예제 2번째

---
+++ _4.1.2_Method2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _4._1._2_Method2
+{
+    class Math
+    {
+        public int GetAera(int x)
+        {
+            return x * x;
+        }
+
+        public int GetValue()
+        {
+            return 10;
+        }
+
+        public void Output(string precon, int value)
+        {
+            Console.WriteLine(precon + value);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = new Math();
+            int result = m.GetAera(m.GetAera(m.GetValue()));
+            
+            m.Output("결과 : ", result);
+        }
+    }
+}
