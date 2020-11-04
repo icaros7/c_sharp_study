2623698c75dd38aaa8a829abc16794c8557d5809
iCAROS7 <hominlab@gmail.com>
2020-11-04 21:11:51 +0900

    4.1.5.1장 정적 필드

---
+++ _4.1.5.1_Static_Field/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _4._1._5_Static_Field
+{
+    class Person
+    {
+        static public int TotalInstance;
+        public string _name;
+
+        public Person(string name)
+        {
+            TotalInstance++;
+            _name = name;
+            Console.WriteLine($"Welecom, " + name + $"!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Homin = new Person("Homin");
+            Console.WriteLine(Person.TotalInstance);
+            
+            var Minho = new Person("Minho");
+            Console.WriteLine(Person.TotalInstance);
+        }
+    }
+}
