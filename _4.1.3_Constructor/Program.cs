bb6234dec5796502395d679f8e345b40de320683
iCAROS7 <hominlab@gmail.com>
2020-11-04 20:25:53 +0900

    4.1.3장 생성자 예제

---
+++ _4.1.3_Constructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _4._1._3_Constructor
+{
+    class Person
+    {
+        private string _name;
+        private int _age;
+        private string _last_name;
+
+        public Person(string name, string lastName, int age)
+        {
+            _name = name;
+            _last_name = lastName;
+            _age = age;
+            Console.WriteLine($"Welecom, " + name + $"!");
+        }
+
+        public void WhoIam() { Console.WriteLine($"My name is " + _last_name + $" " + _name + $" and I am " + _age + $" years old."); }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("호민", "이", 23);
+            person.WhoIam();
+        }
+    }
+}
