63112049156037eec5eb24135ee707150484eab6
iCAROS7 <hominlab@gmail.com>
2020-11-19 20:40:14 +0900

    4.5.1.4장 인터페이스5 느슨한 결합

---
+++ _4.5.1.4_Loosing_Coupling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _4._5._1._4_Loosing_Coupling
+{
+    interface IPower
+    {
+        void TurnOn();
+    }
+
+    class Monitor : IPower
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Monitor : Turn On!");
+        }
+    }
+
+    class Computer : IPower
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Computer : Turn On!");
+        }
+    }
+
+    class Switch
+    {
+        public void PowerOn(IPower machine)
+        {
+            machine.TurnOn();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Monitor monitor = new Monitor();
+            Computer computer = new Computer();
+            Switch sw1 = new Switch();
+            sw1.PowerOn(computer);
+            sw1.PowerOn(monitor);
+        }
+    }
+}
